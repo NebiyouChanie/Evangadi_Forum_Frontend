8cbc6c1e556ec454effefdd6bfd5fb6b7a8cc1b6
Nebiyou <cnebiyu@gmail.com>
2024-10-14 17:41:34 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className='min-h-screen flex flex-col'>
       <Nav />
-      <Questions className='flex-grow'/>
+      <Questions />
       <Footer />
     </div>
   )
